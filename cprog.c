5a604e6cc4dd2bc6e38ffd570d3243b8d735456a
VarshiniRajendran <varshini.rajendran@vvdntech.in>
2018-06-18 15:47:31 +0530

    Adding a new c program

---
+++ cprog.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+
+    int i, space, rows, k=0, count = 0, count1 = 0;
+
+    print("Enter number of rows: ");
+    scanf("%d",&rows);
+
+    for(i=1; i<=rows; ++i)
+
+    {
+        for(space=1; space <= rows-i; ++space)
+        {
+            print("  ");
+            ++count;
+        }
+
+        while(k != 2*i-1)
+        {
+            if (count <= rows-1)
+            {
+                print("%d ", i+k);
+                ++count;
+            }
+            else
+            {
+                ++count1;
+                print("%d ", (i+k-2*count1));
+            }
+            ++k;
+        }
+        count1 = count = k = 0;
+
+        print("\n");
+    }
+    return 0;
+ }
